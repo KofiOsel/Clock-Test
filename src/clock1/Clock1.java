e1f9663406e389a5195326f782bdd198ae52fec3
KofiOsel <72944024+KofiOsel@users.noreply.github.com>
2020-11-30 19:19:15 -0500

    Second commit with Clock1 using NetBeans.

--- src/clock1/Clock1.java
+++ src/clock1/Clock1.java
@@ -10,7 +10,61 @@ package clock1;
  * @author Kofi Osel
  */
 public class Clock1 {
+    
+    private int hour;
+    private int minute;
+    private int second;
 
+    public Clock1() {
+        this.hour = 0;
+        this.minute = 0;
+        this.second = 0;
+    }
+
+    public Clock1(int hour, int minute, int second) {
+        this.hour = hour;
+        this.minute = minute;
+        this.second = second;
+    }
     
-    
+    public Clock1(Clock1 clock1) {
+        this.hour = clock1.hour;
+        this.minute = clock1.minute;
+        this.second = clock1.second;
+    }
+
+    public boolean equals(Clock1 clock1) {
+        return this.hour == clock1.hour &&
+                this.minute == clock1.minute &&
+                this.second == clock1.second;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%02d:%02d:%02d",hour, minute, second); 
+    }
+
+    public int getHour() {
+        return hour;
+    }
+
+    public void setHour(int hour) {
+        this.hour = hour;
+    }
+
+    public int getMinute() {
+        return minute;
+    }
+
+    public void setMinute(int minute) {
+        this.minute = minute;
+    }
+
+    public int getSecond() {
+        return second;
+    }
+
+    public void setSecond(int second) {
+        this.second = second;
+    }   
 }
